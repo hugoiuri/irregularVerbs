ff79a192a2f62bafcf253ddbd7e8f7778567d576
Hugo Iuri Cordeiro <525977@sga.pucminas.br>
2018-08-09 21:40:53 -0300

    first commit

---
+++ base.js
@@ -0,0 +1,23 @@
+var verbs = [
+    {
+        'infinitive': 'arise',
+        'past': 'arose', 
+        'participle': 'arisen'
+    },{
+        'infinitive': 'be',
+        'past': 'was/were', 
+        'participle': 'been'
+    },{
+        'infinitive': 'catch',
+        'past': 'caught', 
+        'participle': 'caught'
+    },{
+        'infinitive': 'have',
+        'past': 'had', 
+        'participle': 'had'
+    },{
+        'infinitive': 'sleep',
+        'past': 'slept', 
+        'participle': 'slept'
+    },
+]
